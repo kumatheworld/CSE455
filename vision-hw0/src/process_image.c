1494be52d16d23d2546e85fbd83bd0aad676525a
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-21 20:25:08 +0900

    Fill rgb_to_grayscale()

--- vision-hw0/src/process_image.c
+++ vision-hw0/src/process_image.c
@@ -37,6 +37,19 @@ image rgb_to_grayscale(image im)
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
     // TODO Fill this in
+    const float w_r = 0.299;
+    const float w_g = 0.587;
+    const float w_b = 0.114;
+
+    for (int y = 0; y < im.h; y++) {
+        for (int x = 0; x < im.w; x++) {
+            int idx_gray = serialize_index(gray, x, y, 0);
+            int idx_r = serialize_index(im, x, y, 0);
+            int idx_g = serialize_index(im, x, y, 1);
+            int idx_b = serialize_index(im, x, y, 2);
+            gray.data[idx_gray] = w_r * im.data[idx_r] + w_g * im.data[idx_g] + w_b * im.data[idx_b];
+        }
+    }
     return gray;
 }
 
