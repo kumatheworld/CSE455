8a9af1dcf110d0dccf5373b18a9a138b10478e8a
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-22 22:02:16 +0900

    Add clamp()

--- vision-hw0/src/process_image.c
+++ vision-hw0/src/process_image.c
@@ -63,6 +63,16 @@ void shift_image(image im, int c, float v)
     }
 }
 
+float clamp(float x) {
+    if (x < 0) {
+        return 0;
+    } else if (x > 1) {
+        return 1;
+    } else {
+        return x;
+    }
+}
+
 void clamp_image(image im)
 {
     // TODO Fill this in
