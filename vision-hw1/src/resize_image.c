fec974480f52a908293d94249c8467a44a4c313c
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-24 20:32:32 +0900

    Generalize nn_interpolate() to resize()

--- vision-hw1/src/resize_image.c
+++ vision-hw1/src/resize_image.c
@@ -1,8 +1,26 @@
 #include <math.h>
 #include "image.h"
 
-float get_coord_for_interp(int z, int l_old, int l_new) {
+float get_coord_for_interp(int z, int l_old, int l_new)
+{
     return (z + .5) * l_old / l_new - .5;
+}
+
+image resize(float (*interpolate)(image im, float x, float y, int c), image im, int w, int h)
+{
+    // TODO
+    image im_new = make_image(w, h, im.c);
+    int idx = 0;
+    for (int c = 0; c < im.c; c++) {
+        for (int y = 0; y < h; y++) {
+            float y_ = get_coord_for_interp(y, im.h, h);
+            for (int x = 0; x < w; x++) {
+                float x_ = get_coord_for_interp(x, im.w, w);
+                im_new.data[idx++] = interpolate(im, x_, y_, c);
+            }
+        }
+    }
+    return im_new;
 }
 
 float nn_interpolate(image im, float x, float y, int c)
@@ -16,18 +34,7 @@ float nn_interpolate(image im, float x, float y, int c)
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    image im_new = make_image(w, h, im.c);
-    int idx = 0;
-    for (int c = 0; c < im.c; c++) {
-        for (int y = 0; y < h; y++) {
-            float y_ = get_coord_for_interp(y, im.h, h);
-            for (int x = 0; x < w; x++) {
-                float x_ = get_coord_for_interp(x, im.w, w);
-                im_new.data[idx++] = nn_interpolate(im, x_, y_, c);
-            }
-        }
-    }
-    return im_new;
+    return resize(*nn_interpolate, im, w, h);
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
