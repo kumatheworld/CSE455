04843a660ba0b67c15b94e10e2f2324b6266f9ed
kumatheworld <kumatheworld1105@yahoo.co.jp>
2020-07-24 20:23:18 +0900

    Fill in nn_resize()

--- vision-hw1/src/resize_image.c
+++ vision-hw1/src/resize_image.c
@@ -16,7 +16,18 @@ float nn_interpolate(image im, float x, float y, int c)
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image im_new = make_image(w, h, im.c);
+    int idx = 0;
+    for (int c = 0; c < im.c; c++) {
+        for (int y = 0; y < h; y++) {
+            float y_ = get_coord_for_interp(y, im.h, h);
+            for (int x = 0; x < w; x++) {
+                float x_ = get_coord_for_interp(x, im.w, w);
+                im_new.data[idx++] = nn_interpolate(im, x_, y_, c);
+            }
+        }
+    }
+    return im_new;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
